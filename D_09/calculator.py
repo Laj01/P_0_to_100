58261fae286965259ecf60169ceac1a64f77bd63
Lajos Nagy <nagylali001@gmail.com>
2023-11-29 14:59:46 +0100

    Add concole calculator boilerplate

---
+++ D_09/calculator.py
@@ -0,0 +1,21 @@
+def main():
+    ...
+    
+def calculator():
+    ...
+
+def add():
+    ...
+
+def subtract():
+    ...
+
+def multiply():
+    ...
+
+def divide():
+    ...
+
+
+if __name__ == '__main__':
+    main()
