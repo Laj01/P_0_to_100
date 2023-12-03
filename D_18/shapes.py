53ed3b6541364fbd8fe4eaf34a7dfb7ebb3d371c
Lajos Nagy <nagylali001@gmail.com>
2023-12-03 18:34:12 +0100

    Add turtle boilerplate

---
+++ D_18/shapes.py
@@ -0,0 +1,14 @@
+from turtle import Turtle, Screen
+
+
+def main():
+    timmy = Turtle()
+    timmy.color('red')
+    timmy.shape('turtle')
+
+
+    timmy.screen.mainloop()
+
+
+if __name__ == '__main__':
+    main()
