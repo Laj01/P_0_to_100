9ebd448dd1b1427319d3aa2cd6e69749f7950e39
Lajos Nagy <nagylali001@gmail.com>
2023-12-03 18:52:36 +0100

    Add draw logic

--- D_18/shapes.py
+++ D_18/shapes.py
@@ -6,6 +6,14 @@ def main():
     timmy.color('red')
     timmy.shape('turtle')
 
+    sides = [3, 4, 5, 6, 7, 8, 9, 10,]
+
+    for side in sides:
+        i = 0
+        while i < side:
+            timmy.forward(100)
+            timmy.right(360 / side)
+            i += 1
 
     timmy.screen.mainloop()
 
