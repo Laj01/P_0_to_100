5ac88a927112b1c677e600400f5d00a22a7ccb90
Lajos Nagy <nagylali001@gmail.com>
2023-12-03 19:26:26 +0100

    import  true random_color to random_walk

--- D_18/walk.py
+++ D_18/walk.py
@@ -1,5 +1,6 @@
 from turtle import Turtle, Screen
 import random
+from color import random_color
 
 
 def main():
@@ -11,10 +12,11 @@ def walk(steps):
     timmy.shape()
     timmy.pensize(15)
     timmy.speed('fast')
+    timmy.screen.colormode(255)
     colors = ['red', 'green', 'blue', 'orange', 'black', 'yellow', 'cyan', 'purple', 'CornflowerBlue','MidnightBlue','GreenYellow','AliceBlue','Lime','SkyBlue','DarkOrange',]
     directions = [0, 90, 180, 270]
     for _ in range(steps):
-        timmy.color(random.choice(colors))
+        timmy.color(random_color())
         timmy.setheading(random.choice(directions))
         timmy.forward(30)
 
