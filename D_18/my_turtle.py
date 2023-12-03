69cffed528a6cbd4bbf747b8817a01d3b3ef78df
Lajos Nagy <nagylali001@gmail.com>
2023-12-03 18:16:52 +0100

    Rename file

---
+++ D_18/my_turtle.py
@@ -0,0 +1,18 @@
+from turtle import Turtle, Screen
+
+
+def main():
+    my_turtle = Turtle()
+    my_turtle.shape('turtle')
+    my_turtle.color('red')
+    for i in range(4):
+        my_turtle.forward(100)
+        my_turtle.left(90)
+
+    my_turtle.screen.mainloop()
+    
+
+
+
+if __name__ == '__main__':
+    main()
