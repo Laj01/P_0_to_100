f30def175c4a46fd5291582eb3e3037e0c264905
Lajos Nagy <nagylali001@gmail.com>
2023-12-03 18:29:33 +0100

    Complete dashed-line

---
+++ D_18/dashed_line.py
@@ -0,0 +1,17 @@
+from turtle import Turtle, Screen
+
+def main():
+    timmy = Turtle()
+    timmy.color('red')
+    timmy.shape('turtle')
+
+    for _ in range(25):
+        timmy.pd()
+        timmy.forward(10)
+        timmy.pu()
+        timmy.forward(10)
+
+
+
+if __name__ == '__main__':
+    main()
