ac9d3f3dc277edeb9a05a9bd559a6eefccad399c
Lajos Nagy <nagylali001@gmail.com>
2023-12-03 18:55:38 +0100

    Add random colors to draw with

--- D_18/shapes.py
+++ D_18/shapes.py
@@ -1,15 +1,17 @@
 from turtle import Turtle, Screen
+import random
 
 
 def main():
-    timmy = Turtle()
-    timmy.color('red')
+    timmy = Turtle() 
     timmy.shape('turtle')
 
+    colors = ['red', 'green', 'blue', 'orange', 'black', 'yellow', 'cyan', 'purple']
     sides = [3, 4, 5, 6, 7, 8, 9, 10,]
 
     for side in sides:
         i = 0
+        timmy.color(random.choice(colors))
         while i < side:
             timmy.forward(100)
             timmy.right(360 / side)
