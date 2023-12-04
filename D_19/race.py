a292b0bd3dad970e2440da951329b1e43814b922
Lajos Nagy <nagylali001@gmail.com>
2023-12-04 14:33:57 +0100

    Add start_race function

--- D_19/race.py
+++ D_19/race.py
@@ -1,16 +1,21 @@
 from turtle import Turtle, Screen
+import random
 
 
 screen = Screen()
 screen.setup(width=800, height=600)
+is_on = False
 bet = screen.textinput(title='Make a bet', prompt='Which color will win the race?').lower()
+if bet: is_on = True
 colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple']
-#timmy = Turtle(shape='turtle')
-#timmy.penup()
+racers = []
+
 
 def main():    
     set_start()
+    start_race()
     screen.exitonclick()
+
 
 def set_start():
     posx = -350
@@ -21,6 +26,22 @@ def set_start():
         turtle.penup()
         turtle.color(colors[i])
         turtle.goto(x=posx, y=posy+(i*gap))
+        racers.append(turtle)
+
+
+def start_race():
+        global is_on
+        while is_on:
+            for racer in racers:
+                if racer.xcor() > 380:
+                    is_on = False
+                    winner_color = racer.pencolor()
+                    if bet == winner_color:
+                        print(f'YOU WON. The {winner_color} turtle won!')
+                    else:
+                        print(f'YOU LOST. The {winner_color} turtle won!')
+                rand_distance = random.randint(0, 10)
+                racer.forward(rand_distance)
 
 
 if __name__ == '__main__':
