74c630d598995f4b4266dc86f21a23a223461d4a
Lajos Nagy <nagylali001@gmail.com>
2023-12-04 15:46:35 +0100

    Screen and starting body commit

--- D_20_snake/main.py
+++ D_20_snake/main.py
@@ -1,12 +1,13 @@
 from turtle import Turtle, Screen
 
-
+# Set up the starting screen
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.cv._rootwindow.resizable(False, False)
 screen.bgcolor('black')
 screen.title('Snake')
 
+# Add the starting snake body to the screen
 start_positions = [(0, 0), (-20, 0), (-40, 0),]
 for position in start_positions:
     part = Turtle(shape='square')
