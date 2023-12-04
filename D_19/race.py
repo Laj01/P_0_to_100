4dfeb4ee0f62c40f73810d759923fec8ee9a3f02
Lajos Nagy <nagylali001@gmail.com>
2023-12-04 14:08:46 +0100

    Add start positions

--- D_19/race.py
+++ D_19/race.py
@@ -1,12 +1,26 @@
 from turtle import Turtle, Screen
 
-timmy = Turtle()
+
 screen = Screen()
 screen.setup(width=800, height=600)
-
 bet = screen.textinput(title='Make a bet', prompt='Which color will win the race?').lower()
-def main():
-    print(bet)
+colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple']
+#timmy = Turtle(shape='turtle')
+#timmy.penup()
+
+def main():    
+    set_start()
+    screen.exitonclick()
+
+def set_start():
+    posx = -350
+    posy = -150
+    gap = 50
+    for i in range(0, 6):
+        turtle = Turtle(shape='turtle')
+        turtle.penup()
+        turtle.color(colors[i])
+        turtle.goto(x=posx, y=posy+(i*gap))
 
 
 if __name__ == '__main__':
