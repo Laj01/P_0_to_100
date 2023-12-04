f184650abef5b45cea50af0d31b2ea7900ed4fea
Lajos Nagy <nagylali001@gmail.com>
2023-12-04 15:38:44 +0100

    Add screen boilerplate for snake game

---
+++ D_20_snake/main.py
@@ -0,0 +1,20 @@
+from turtle import Turtle, Screen
+
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.cv._rootwindow.resizable(False, False)
+screen.bgcolor('black')
+screen.title('Snake')
+
+
+
+
+
+
+
+
+
+
+
+screen.exitonclick()
