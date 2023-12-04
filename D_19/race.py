2697455bfedfa5771063e9f69a98c0d5a0763607
Lajos Nagy <nagylali001@gmail.com>
2023-12-04 13:39:36 +0100

    Add pop-up window for user bet

--- D_19/race.py
+++ D_19/race.py
@@ -2,10 +2,11 @@ from turtle import Turtle, Screen
 
 timmy = Turtle()
 screen = Screen()
+screen.setup(width=800, height=600)
 
-
+bet = screen.textinput(title='Make a bet', prompt='Which color will win the race?').lower()
 def main():
-    ...
+    print(bet)
 
 
 if __name__ == '__main__':
