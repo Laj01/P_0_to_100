d28f1628e2e1ae204c0d96154882e1ca95442b9b
Lajos Nagy <nagylali001@gmail.com>
2023-12-06 13:14:04 +0100

    Complete mail merger

--- D_24_mail_merge/main.py
+++ D_24_mail_merge/main.py
@@ -1,5 +1,15 @@
-def main():
-    ...
+def main():       
+    with open('Input/Names/names.txt', mode='r') as names_file:
+        names = names_file.readlines()      
+
+    with open('Input/Letters/starting_letter.txt', mode='r') as letter_file:
+        lines = letter_file.readlines()
+        
+    for name in names:
+        for line in lines:
+            new_line = line.replace('[name]', name.strip())            
+            with open(f'Output/ReadyToSend/Letter_for_{name.strip()}.txt', mode='a') as output:
+                output.write(new_line)
 
 
 if __name__ == '__main__':
