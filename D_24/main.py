41d305cc4654cc794bae4e16c2083294dcc995f7
Lajos Nagy <nagylali001@gmail.com>
2023-12-06 11:44:50 +0100

    Append to file

--- D_24/main.py
+++ D_24/main.py
@@ -2,5 +2,8 @@
     content = file.read()
     print(content) """
 
-with open('text_note2.txt', mode='w') as file:
-    file.write('Replace text.')
+""" with open('text_note2.txt', mode='w') as file:
+    file.write('Replace text.') """
+
+with open('text_note2.txt', mode='a') as file:
+    file.write('\nNext line.')
