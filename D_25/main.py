90fb7330e959695fd8def8e003c61f1a7ad3b77d
Lajos Nagy <nagylali001@gmail.com>
2023-12-07 15:25:20 +0100

    Add loop for continuous guessing

--- D_25/main.py
+++ D_25/main.py
@@ -5,30 +5,31 @@ SCREEN_PATH = 'blank_states_img.gif'
 DATA_PATH = '50_states.csv'
 
 screen = turtle.Screen()
+screen.setup(width=800, height=600)
 screen.title('US States')
 image = SCREEN_PATH
 screen.addshape(image)
 turtle.shape(image)
-
 data = pd.read_csv(DATA_PATH, sep=',')
-
-
-correct_answers = 0
-popup_title = f'{correct_answers}/50 States correct'
-answer = screen.textinput(title=popup_title, prompt='Guess the name of the states:')
-titled_answer = answer.title()
 
 states = data['state'].to_list()
 
+guessed_states = []
 
-if titled_answer in states:
-    t = turtle.Turtle()
-    t.penup()
-    t.hideturtle()
-    state_data = data[data['state'] == titled_answer]
-    t.goto(int(state_data.x), int(state_data.y))    
-    t.write(titled_answer)
-    correct_answers += 1
+while len(guessed_states) < 50:
+    popup_title = f'{len(guessed_states)}/50 States correct'
+    answer = screen.textinput(title=popup_title, prompt='Guess the name of the states:').title()
+
+
+    if answer in states:
+        t = turtle.Turtle()
+        t.penup()
+        t.hideturtle()
+        state_data = data[data['state'] == answer]
+        t.goto(int(state_data['x']), int(state_data['y']))    
+        t.write(answer)
+        guessed_states.append(answer)
+        
 
 
 turtle.mainloop()
