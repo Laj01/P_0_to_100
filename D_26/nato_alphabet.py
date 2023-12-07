71862ef01ce4147a92c0523b402bc5432b4984e0
Lajos Nagy <nagylali001@gmail.com>
2023-12-07 18:43:39 +0100

    Create nato-letter-dictionary from dataframe

--- D_26/nato_alphabet.py
+++ D_26/nato_alphabet.py
@@ -3,5 +3,10 @@ import pandas as pd
 
 fp = 'nato_phonetic_alphabet.csv'
 df = pd.read_csv(fp, sep=',')
-dict_df = df.to_dict()
-print(dict_df)
+#dict_df = df.to_dict()
+
+nato_dict = {row['letter']:row['code'] for (index, row) in df.iterrows()}
+
+user_input = input('Enter your name: ')
+user_letters = [letter for letter in user_input]
+print(user_letters)
