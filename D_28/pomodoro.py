9cfd98874897a08b036ed0db3e224cd209341e53
Lajos Nagy <nagylali001@gmail.com>
2023-12-09 13:05:07 +0100

    Add reset for timer

--- D_28/pomodoro.py
+++ D_28/pomodoro.py
@@ -19,6 +19,17 @@ SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 SEC = 60
 reps = 0
+timer = None
+
+
+## Reset timer ##
+def reset_timer():
+    window.after_cancel(timer)
+    canvas.itemconfig(timer_text, text='00:00')
+    title_label.config(text='TIMER')
+    check_marks.config(text='')
+    global reps
+    reps = 0
 
 
 ## Timer ##
@@ -42,7 +53,8 @@ def count_down(count):
     counter_seconds = count % 60
     canvas.itemconfig(timer_text, text = f'{counter_minutes:02d}:{counter_seconds:02d}')
     if count > 0:
-        window.after(10, count_down, count - 1)
+        global timer
+        timer = window.after(10, count_down, count - 1)
     else:
         start_timer()
         marks = ''
@@ -50,7 +62,6 @@ def count_down(count):
         for _ in range(sessions):
             marks += CHECK_MARK
         check_marks.config(text=marks)
-
 
 
 ## UI ##
@@ -65,7 +76,7 @@ title_label.grid(column=1, row=0)
 start_button = Button(text='Start', highlightthickness=0, command=start_timer)
 start_button.grid(column=0, row=2)
 
-reset_button = Button(text='Reset', highlightthickness=0)
+reset_button = Button(text='Reset', highlightthickness=0, command=reset_timer)
 reset_button.grid(column=2, row=2)
 
 check_marks = Label(fg=GREEN, bg=YELLOW)
