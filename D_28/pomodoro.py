a362247cbf93a39a27e2e34015ba7eba7c1b01b0
Lajos Nagy <nagylali001@gmail.com>
2023-12-09 11:03:56 +0100

    Add constants and set timer position

--- D_28/pomodoro.py
+++ D_28/pomodoro.py
@@ -1,5 +1,20 @@
 from tkinter import *
 
+#Fonts
+FONT_NAME = 'Arial'
+FONT_SIZE = 30
+FONT_STYLE = 'bold'
+
+#Colors
+PINK = "#e2979c"
+RED = "#e7305b"
+GREEN = "#9bdeac"
+YELLOW = "#f7f5dd"
+
+#Timer
+WORK_MIN = 25
+SHORT_BREAK_MIN = 5
+LONG_BREAK_MIN = 20
 
 window = Tk()
 window.title('Pomodoro')
@@ -9,7 +24,7 @@ window.config(padx=100, pady=50)
 canvas = Canvas(width=200, height=224)
 tomato_img = PhotoImage(file='tomato.png')
 canvas.create_image(102, 112, image = tomato_img)
-
+canvas.create_text(102, 130, text='00:00', fill='white', font=(FONT_NAME, FONT_SIZE, FONT_STYLE))
 canvas.pack()
 
 
