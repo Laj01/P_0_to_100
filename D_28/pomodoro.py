53bc897907de98d070911b4ad968d8cebe172f81
Lajos Nagy <nagylali001@gmail.com>
2023-12-09 10:59:07 +0100

    Set background img to center

--- D_28/pomodoro.py
+++ D_28/pomodoro.py
@@ -4,9 +4,13 @@ from tkinter import *
 window = Tk()
 window.title('Pomodoro')
 window.resizable(False, False)
+window.config(padx=100, pady=50)
 
 canvas = Canvas(width=200, height=224)
+tomato_img = PhotoImage(file='tomato.png')
+canvas.create_image(102, 112, image = tomato_img)
 
+canvas.pack()
 
 
 window.mainloop()
