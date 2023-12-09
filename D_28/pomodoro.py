f585dbe95481bd3bb1b4f651ee23f85397de9bf7
Lajos Nagy <nagylali001@gmail.com>
2023-12-09 12:29:03 +0100

    Add logic to count down timer

--- D_28/pomodoro.py
+++ D_28/pomodoro.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import math
 
 #Fonts
 FONT_NAME = 'Arial'
@@ -17,6 +18,19 @@ WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 
+def start_timer():
+    count_down(100)
+
+## Counting down ##
+def count_down(count):
+    counter_minutes = math.floor(count / 60)
+    counter_seconds = count % 60
+    canvas.itemconfig(timer_text, text = f'{counter_minutes:02d}:{counter_seconds:02d}')
+    if count > 0:
+        window.after(1000, count_down, count - 1)
+
+
+## UI ##
 window = Tk()
 window.title('Pomodoro')
 window.resizable(False, False)
@@ -25,7 +39,7 @@ window.config(padx=100, pady=50, bg=YELLOW)
 title_label = Label(text='TIMER', fg=GREEN, bg=YELLOW, font=(FONT_NAME, FONT_SIZE, FONT_STYLE))
 title_label.grid(column=1, row=0)
 
-start_button = Button(text='Start', highlightthickness=0)
+start_button = Button(text='Start', highlightthickness=0, command=start_timer)
 start_button.grid(column=0, row=2)
 
 reset_button = Button(text='Reset', highlightthickness=0)
@@ -37,7 +51,7 @@ check_marks.grid(column=1, row=2)
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file='tomato.png')
 canvas.create_image(100, 112, image = tomato_img)
-canvas.create_text(102, 130, text='00:00', fill='white', font=(FONT_NAME, FONT_SIZE, FONT_STYLE))
+timer_text = canvas.create_text(102, 130, text='00:00', fill='white', font=(FONT_NAME, FONT_SIZE, FONT_STYLE))
 canvas.grid(column=1, row=1)
 
 
