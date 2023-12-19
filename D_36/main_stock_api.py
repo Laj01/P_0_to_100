a37f7e7634b768198aa90b9b1375f9d11ab6d447
Lajos Nagy <nagylali001@gmail.com>
2023-12-19 21:31:01 +0100

    Calculate the percentage difference in stock price

--- D_36/main_stock_api.py
+++ D_36/main_stock_api.py
@@ -14,7 +14,23 @@ def main():
 
     response = requests.get(STOCK_ENDPOINT, params=stock_params)
     response.raise_for_status()
-    print(response.json())
+    data = response.json()['Time Series (Daily)']
+    data_list = [value for (key, value) in data.items()]
+
+    #get yesterday's closing price
+    yesterday_data = data_list[0]
+    y_closing_price = yesterday_data['4. close']
+
+    #the day before yesterday's closing price
+    day_before_yesterday_data = data_list[1]
+    d_b_y_closing_price = day_before_yesterday_data['4. close']
+
+    #Calculate the percentage difference between the 2 prices
+    difference = abs(float(y_closing_price) - float(d_b_y_closing_price))
+    diff_percentage = (difference / float(y_closing_price)) * 100
+
+
+    print(diff_percentage)
 
 
 if __name__ == '__main__':
