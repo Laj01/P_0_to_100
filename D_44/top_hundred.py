2d5e90fdc64488dcf981cb68f6f0d4cea3f935cd
Lajos Nagy <nagylali001@gmail.com>
2024-01-08 17:00:59 +0100

    Scrape movie list from site

--- D_44/top_hundred.py
+++ D_44/top_hundred.py
@@ -4,9 +4,18 @@ from bs4 import BeautifulSoup
 URL = 'https://www.empireonline.com/movies/features/best-movies-2/'
 
 def main():
-    pass
+    response = requests.get(URL)
+    website_html = response.text
+
+    soup = BeautifulSoup(website_html, 'html.parser')
+    all_movies = soup.find_all(name='h3', class_='listicleItem_listicle-item__title__BfenH')
+    movie_titles = [movie.getText() for movie in all_movies]
+    #movies_ordered = movie_titles[::-1]
+    movie_titles.reverse()
+
+    print(movie_titles)
 
 
 
-if __name__ == __main__:
+if __name__ == '__main__':
     main()
