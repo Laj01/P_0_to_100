f0878fdece88bb27925619bae19da83af8faa0c9
Lajos Nagy <nagylali001@gmail.com>
2024-01-08 16:47:30 +0100

    Add boilerplate

---
+++ D_44/top_hundred.py
@@ -0,0 +1,12 @@
+import requests
+from bs4 import BeautifulSoup
+
+URL = 'https://www.empireonline.com/movies/features/best-movies-2/'
+
+def main():
+    pass
+
+
+
+if __name__ == __main__:
+    main()
